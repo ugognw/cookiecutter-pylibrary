3dbcc517014fb077dd4c3e9366f7326957559349
Ugochukwu Nwosu <ugognw@gmail.com>
2023-07-07 11:19:14 -0600

    Add cwd global to pylibrary
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- pylibrary.py
+++ pylibrary.py
@@ -1,5 +1,7 @@
 from json import dumps
+import pathlib
 
+from jinja2 import Environment
 from jinja2.ext import Extension
 
 
@@ -8,6 +10,7 @@ def jsonquote(value):
 
 
 class JsonQuoteExtension(Extension):
-    def __init__(self, environment):
+    def __init__(self, environment: Environment):
         super().__init__(environment)
         environment.filters['jsonquote'] = jsonquote
+        environment.globals['cwd'] = pathlib.Path.cwd().as_posix()
