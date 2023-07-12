a0b595f1f419aef5d3e196ae0c0d61f1b31f5c52
Ugochukwu Nwosu <ugognw@gmail.com>
2023-07-11 20:35:41 -0600

    Initialize project in post hook
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -58,62 +58,111 @@ if __name__ == "__main__":
     width = min(140, shutil.get_terminal_size(fallback=(140, 0)).columns)
 {%- if cookiecutter.initialize_git_repository == 'yes' %}
     note(' Initializing Git repository '.center(width, "#"))
-    subprocess.check_call(['git', 'init'])
+    _ = subprocess.check_call(['git', 'init'])
+    _ = subprocess.check_call(['git', 'add', '--all'])
+    _ = subprocess.check_call(['git', 'commit', ' -m', '"Add initial project skeleton."'])
+    _ = subprocess.check_call(['git', 'tag', ' -m', 'v{{ cookiecutter.version }}'])
+    _ = subprocess.check_call(['git', 'remote', 'add', 'origin', 'git@{{ cookiecutter.repo_hosting }}:{{ cookiecutter.repo_username }}/{{ cookiecutter.repo_name }}.git'])
+    _ = subprocess.check_call(['git', 'push', ' -u', 'origin', '{{ cookiecutter.repo_main_branch }} v{{ cookiecutter.version }}'])
 {%- endif %}
-
+    package_installed = False
+    venv_activated = False
 {% if cookiecutter.install_package == 'yes' %}
     note(' Installing package and dependencies '.center(width, "#"))
     try:
         _ = subprocess.check_call(['poetry', 'install'])
+        package_installed = True
 
 {%- if cookiecutter.activate_virtual_environment %}
         note(' Activating virtual environment '.center(width, "#"))
         _ = subprocess.check_call(['source', '"$(poetry env info --path)"/bin/activate'])
+        venv_activated = True
 {%- endif %}
-    except FileNotFoundError as err:
-        if 'no such file' in err.args[1].lower():
-            note('Installing poetry'.center(width, "#"))
-            _ = subprocess.check_call(
-                [
-                    'curl',
-                    '-sSL',
-                    'https://install.python-poetry.org',
-                    '|',
-                    'python3',
-                    '-'
-                ]
-            )
-            note('Poetry successfully installed'.center(width, "#"))
-        else:
+    except FileNotFoundError:
+        note('Installing poetry'.center(width, "#"))
+        _ = subprocess.check_call(
+            [
+                'curl',
+                '-sSL',
+                'https://install.python-poetry.org',
+                '|',
+                'python3',
+                '-'
+            ]
+        )
+        note('Poetry successfully installed'.center(width, "#"))
+        _ = subprocess.check_call(['poetry', 'install'])
+        package_installed = True
+{% if cookiecutter.activate_virtual_environment %}
+        note(' Activating virtual environment '.center(width, "#"))
+        _ = subprocess.check_call(['source', '"$(poetry env info --path)"/bin/activate'])
+        venv_activated = True
+{%- endif %}
+    except Exception:
             warn(
-                'Unable to install Poetry. You will need to install Poetry in order to install the project and activate the virtual environment. https://python-poetry.org/docs/#installation.'
+                'Unable to install Poetry.'.center(width, "#")
+            )
+            warn(
+                'You will need to install Poetry in order to install the project and activate the virtual environment.'.center(width, "#")
+            )
+            warn(
+                'https://python-poetry.org/docs/#installation.'.center(width, "#")
             )
 {%- endif %}
-
+    pre_commit_installed = False
 {%- if cookiecutter.pre_commit == 'no' %}
     cwd.joinpath('.pre-commit-config.yaml').unlink()
 {%- elif cookiecutter.install_precommit_hooks %}
     note(' Setting up pre-commit '.center(width, "#"))
     if cwd.joinpath('.git').exists():
-        subprocess.check_call(['pre-commit', 'install', '--install-hooks'])
-        subprocess.check_call(['pre-commit', 'autoupdate'])
+        try:
+            _ = subprocess.check_call(['pre-commit', 'install', '--install-hooks'])
+            _ = subprocess.check_call(['pre-commit', 'autoupdate'])
+            pre_commit_installed = True
+        except FileNotFoundError:
+            try:
+                _ = subprocess.check_call(['pip', 'install', 'pre-commit'])
+            except Exception:
+                warn(
+                'Unable to install pre-commit.'.center(width, "#")
+                )
+
     else:
         print('Skipping precommit install.')
 {%- endif %}
     success(' Successfully created `{{ cookiecutter.repo_name }}` '.center(width, "#"))
     print('See .cookiecutterrc for instructions on regenerating the project.')
     note('To get started run these:')
-    print('''
-cd {{ cookiecutter.repo_name }}
-git init
-pre-commit install --install-hooks
-pre-commit autoupdate
-git add --all
-git commit -m "Add initial project skeleton."
-git tag v{{ cookiecutter.version }}
-git remote add origin git@{{ cookiecutter.repo_hosting }}:{{ cookiecutter.repo_username }}/{{ cookiecutter.repo_name }}.git
-git push -u origin {{ cookiecutter.repo_main_branch }} v{{ cookiecutter.version }}
-''')
+    commands = [
+        'cd {{ cookiecutter.repo_name }}',
+{%- if cookiecutter.initialize_git_repository == 'no' %}
+        'git init',
+{%- endif %}
+    ]
+{% if cookiecutter.pre_commit == 'yes' %}
+    if not pre_commit_installed:
+        commands.extend(('pre-commit install --install-hooks', 'pre-commit autoupdate'))
+{% endif %}
+
+    if not package_installed:
+        commands.extend('poetry install')
+
+    if not venv_activated:
+        commands.extend('source "$(poetry env info --path)"/bin/activate')
+
+{%- if cookiecutter.initialize_git_repository == 'no' %}
+    commands.extend(
+        (
+            'git add --all',
+            'git commit -m "Add initial project skeleton."',
+            'git tag v{{ cookiecutter.version }}',
+            'git remote add origin git@{{ cookiecutter.repo_hosting }}:{{ cookiecutter.repo_username }}/{{ cookiecutter.repo_name }}.git',
+            'git push -u origin {{ cookiecutter.repo_main_branch }} v{{ cookiecutter.version }}'
+        )
+    )
+{%- endif %}
+
+    print('\n'.join(commands))
     cli_bin_name = '{{ cookiecutter.cli_bin_name }}'
     while cli_bin_name.endswith('.py'):
         cli_bin_name = cli_bin_name[:-3]
