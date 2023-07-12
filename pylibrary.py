413db73af77adf5a54dc0ee5053d66db2a046cb5
Ugochukwu Nwosu <ugognw@gmail.com>
2023-07-12 10:33:20 -0600

    Remove cwd global
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- pylibrary.py
+++ pylibrary.py
@@ -1,5 +1,4 @@
 from json import dumps
-import pathlib
 
 from jinja2 import Environment
 from jinja2.ext import Extension
@@ -13,4 +12,3 @@ class JsonQuoteExtension(Extension):
     def __init__(self, environment: Environment):
         super().__init__(environment)
         environment.filters['jsonquote'] = jsonquote
-        environment.globals['cwd'] = pathlib.Path.cwd().as_posix()
